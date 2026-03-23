fabdfe723483629fceb3d7080fb919971a69bbf6
BolasLien <bolaslien@gmail.com>
2026-03-23 23:14:11 +0800

    perf(scripts): 將 jQuery/moment/column.js 改為 defer 載入消除 render-blocking
    - jQuery 3.3.1、moment 2.22.2、clipboard.js、column.js 全部加上 defer
    - 移除同步 moment.locale() inline script，改由 IcarusThemeSettings.locale 傳入
    - 調整 script 順序：IcarusThemeSettings 內嵌 inline 放最前，deferred scripts 依序排列
    
    Co-Authored-By: Paperclip <noreply@paperclip.ing>

--- themes/icarus/layout/common/scripts.jsx
+++ themes/icarus/layout/common/scripts.jsx
@@ -25,16 +25,16 @@ module.exports = class extends Component {
                     clipboard: ${clipboard},
                     fold: '${fold}'
                 }
-            }
+            },
+            locale: '${language}'
         };`;
 
         return <Fragment>
-            <script src={cdn('jquery', '3.3.1', 'dist/jquery.min.js')}></script>
-            <script src={cdn('moment', '2.22.2', 'min/moment-with-locales.min.js')}></script>
-            {clipboard && <script src={cdn('clipboard', '2.0.4', 'dist/clipboard.min.js')} defer></script>}
-            <script dangerouslySetInnerHTML={{ __html: `moment.locale("${language}");` }}></script>
             <script dangerouslySetInnerHTML={{ __html: embeddedConfig }}></script>
-            <script src={url_for('/js/column.js')}></script>
+            <script src={cdn('jquery', '3.3.1', 'dist/jquery.min.js')} defer></script>
+            <script src={cdn('moment', '2.22.2', 'min/moment-with-locales.min.js')} defer></script>
+            {clipboard && <script src={cdn('clipboard', '2.0.4', 'dist/clipboard.min.js')} defer></script>}
+            <script src={url_for('/js/column.js')} defer></script>
             <Plugins site={site} config={config} page={page} helper={helper} head={false} />
             <script src={url_for('/js/main.js')} defer></script>
         </Fragment>;
